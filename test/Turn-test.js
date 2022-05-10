b945a104556d86d617befb3368ebc474adb6e82c
Kendall McGree <mcgreekendall@gmail.com>
2022-05-09 20:05:11 -0700

    add tests for all turn class methods, all tests pass

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,6 +7,7 @@ const Turn = require("../src/Turn");
 describe("Turn", () => {
   let turn;
   let card;
+  let turn2;
   beforeEach(() => {
     card = new Card(
       18,
@@ -15,6 +16,7 @@ describe("Turn", () => {
       "current element"
     );
     turn = new Turn("slice()", card);
+    turn2 = new Turn("current element", card);
   });
 
   it("Should be a function", () => {
@@ -31,5 +33,29 @@ describe("Turn", () => {
 
   it("Should store an instance of Card", () => {
     expect(turn.card).to.equal(card);
+  });
+
+  it("Should have a method that returns the user's guess", () => {
+    expect(turn.returnGuess()).to.equal("slice()");
+  });
+
+  it("Should have a method that returns the card", () => {
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it("Should have a method that returns false for the wrong answer", () => {
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it("Should have a method that returns true for the correct answer", () => {
+    expect(turn2.evaluateGuess()).to.equal(true);
+  });
+
+  it("Should have a method that gives the user feedback if incorrect", () => {
+    expect(turn.giveFeedback()).to.equal("incorrect!");
+  });
+
+  it("Should have a method that gives the user feedback if correct", () => {
+    expect(turn2.giveFeedback()).to.equal("correct!");
   });
 });
